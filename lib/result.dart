bb2b2872d45b85fc25eafaf1f088d65cec98fb00
Tommy Chow <ckchowai@connect.ust.hk>
2022-10-12 00:06:53 +0800

    fix bug

--- lib/result.dart
+++ lib/result.dart
@@ -9,17 +9,6 @@ class Result extends StatelessWidget {
   String get resultPhrase {
     var resultText =
         "You have finished.\n Your score is : " + resultScore.toString();
-    /*
-    if (resultScore <= 8) {
-      resultText = "You are awesome and innocent!";
-    } else if (resultScore <= 12) {
-      resultText = "Pretty likeable!";
-    } else if (resultScore <= 16) {
-      resultText = "You are ... strange?!";
-    } else {
-      resultText = "You are so bad!";
-    }
-    */
     return resultText;
   }
 
